0d86c76e13b07b379088250fd10aeba0fb25e51c
Emily Ng <ngem3@oocl.com>
2024-11-25 15:05:19 +0800

    feat: get all employees

---
+++ src/main/java/com/oocl/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.oocl.springbootemployee.controller;
+
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeRepository employeeRepository = new EmployeeRepository();
+
+    @GetMapping(path = "")
+    public List<Employee> getAll() {
+        return employeeRepository.getAll();
+    }
+}
---
+++ src/main/java/com/oocl/springbootemployee/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.oocl.springbootemployee.repository;
+
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.model.Gender;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    private List<Employee> employees = new ArrayList<>();
+
+    public EmployeeRepository(){
+        employees.add(new Employee(1, "Emily", 10, Gender.FEMALE, 10000.0));
+        employees.add(new Employee(2, "Angus", 10, Gender.MALE, 10000.0));
+    }
+    public List<Employee> getAll() {
+        return employees;
+    }
+}

e0ad3a4211e2e0905886738b59091e33b5e7fb08
Emily Ng <ngem3@oocl.com>
2024-11-25 16:11:17 +0800

    refactor: use jacksonTester for assertion

--- src/test/java/com/oocl/springbootemployee/EmployeeControllerTests.java
+++ src/test/java/com/oocl/springbootemployee/EmployeeControllerTests.java
@@ -4,16 +4,24 @@ import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.BasicJsonTester;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+
+@AutoConfigureJsonTesters
 public class EmployeeControllerTests {
 
     @Autowired
@@ -22,24 +30,26 @@ public class EmployeeControllerTests {
     private MockMvc mockMvc;
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private JacksonTester<List<Employee>> listJson;
+    @Autowired
+    private JacksonTester<Employee> json;
 
     @Test
     public void should_retrun_all_employees_when_get_all() throws Exception {
 
         List<Employee> expected_employees = employeeRepository.getAll();
 
-        client.perform(MockMvcRequestBuilders.get("/employees"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(expected_employees.get(0).getName()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(expected_employees.get(1).getName()));
+        String employeeListJson = client.perform(MockMvcRequestBuilders.get("/employees")).andReturn().getResponse().getContentAsString();
+        assertThat(listJson.parse(employeeListJson)).usingRecursiveComparison().isEqualTo(expected_employees);
     }
+
     @Test
     public void should_retrun_employee_id1_when_getById_given_id1() throws Exception {
 
         Employee expected_employee = employeeRepository.getById(1);
 
-        client.perform(MockMvcRequestBuilders.get("/employees/1"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(expected_employee.getName()));
+        String employeeJson = client.perform(MockMvcRequestBuilders.get("/employees/1")).andReturn().getResponse().getContentAsString();
+        assertThat(json.parse(employeeJson)).usingRecursiveComparison().isEqualTo(expected_employee);
     }
 }

969b1dba3b4e924653538f772e4029c0e1171810
Emily Ng <ngem3@oocl.com>
2024-11-25 15:25:40 +0800

    test: get all employees

---
+++ src/test/java/com/oocl/springbootemployee/EmployeeControllerTests.java
@@ -0,0 +1,36 @@
+package com.oocl.springbootemployee;
+
+import com.oocl.springbootemployee.model.Employee;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTests {
+
+    @Autowired
+    private MockMvc client;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void should_retrun_all_employees_when_get_all() throws Exception {
+
+        List<Employee> expected_employees = employeeRepository.getAll();
+
+        client.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value(expected_employees.get(0).getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value(expected_employees.get(1).getName()));
+    }
+}
